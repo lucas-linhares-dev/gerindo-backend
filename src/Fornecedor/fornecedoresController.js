11dc9e7709348aa0307718ca71836e02676c57e6
Lucas <lucas_linhares_@hotmail.com>
2023-04-17 19:03:44 -0300

    Ajustes fornecedor

--- src/Fornecedor/fornecedoresController.js
+++ src/Fornecedor/fornecedoresController.js
@@ -27,47 +27,52 @@ router.post('/fornecedores', async (req, res) => {
     }
   })
 
-// router.get('/fornecedores/nome', async (req, res) => {
-//     const {nome} = req.query;
-//     const fornecedores = await Fornecedor.find()
+router.get('/fornecedores_filter_name', async (req, res) => {
 
-//     const fornecedoresFiltrados = fornecedores.filter((fornecedor) => fornecedor.nome.toLowerCase().includes(nome.toLowerCase()))
+    const {nome} = req.query;
+    const fornecedores = await Fornecedor.find()
 
-//     console.log("________________________")
-//     console.log(fornecedoresFiltrados)
-//     console.log("________________________")
+    const fornecedoresFiltrados = fornecedores.filter((fornecedor) => fornecedor.nome.toLowerCase().includes(nome.toLowerCase()))
+
+    console.log("________________________")
+    console.log(fornecedoresFiltrados)
+    console.log("________________________")
+
+    const objResponse = {
+      fornecedores: fornecedoresFiltrados,
+      length: fornecedoresFiltrados.length
+    }
       
-//     if(fornecedoresFiltrados.length === 0){
-//     console.log("FORNECEDOR NAO ENCONTRADO")
-//     res.json([])
-//     } else {
-//     res.status(200).json(fornecedoresFiltrados)
-//     }
-// })
-
-// router.get('/fornecedores', async (req, res) => {
-//   console.log("ENTROU AQUI")
-//   const fornecedores = await Fornecedor.find()
-//   res.json(fornecedores)
-// })
-
-router.get('/fornecedores/id', async (req, res) => {
-  const {id} = req.query;
-  console.log(id)
-
-  const fornecedor = await Fornecedor.findOne({_id: id}) 
-
-  console.log("________________________")
-  console.log(fornecedor)
-  console.log("________________________")
-
-  if(fornecedoresFiltrados.length === 0){
+    if(fornecedoresFiltrados.length === 0){
     console.log("FORNECEDOR NAO ENCONTRADO")
     res.json([])
-
-  } else {
-    res.status(200).json(fornecedor)
-  }
+    } else {
+    res.status(200).json(objResponse)
+    }
 })
+
+router.get('/fornecedores', async (req, res) => {
+  const fornecedores = await Fornecedor.find()
+  res.json(fornecedores)
+})
+
+// router.get('/fornecedores_filter_id', async (req, res) => {
+//   const {id} = req.query;
+//   console.log(id)
+
+//   const fornecedor = await Fornecedor.findOne({_id: id}) 
+
+//   console.log("________________________")
+//   console.log(fornecedor)
+//   console.log("________________________")
+
+//   if(fornecedoresFiltrados.length === 0){
+//     console.log("FORNECEDOR NAO ENCONTRADO")
+//     res.json([])
+
+//   } else {
+//     res.status(200).json(fornecedor)
+//   }
+// })
 
 module.exports = router
